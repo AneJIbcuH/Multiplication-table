e3d8cd69057c559219ba1151d188eb93b8ccdb07
AneJIbcuH <114512931+AneJIbcuH@users.noreply.github.com>
2023-08-14 22:47:11 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import SignUp from './SignUp'
+import Game from './Game'
 
 function App() {
 
   return (
     <div className='app'>
       <SignUp></SignUp>
+      <Game></Game>
     </div>
   )
 }
