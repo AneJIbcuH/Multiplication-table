8f9d78243a548fcdcd641c6167d1f3d96201cfc9
AneJIbcuH <114512931+AneJIbcuH@users.noreply.github.com>
2023-08-18 17:27:32 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import SignUp from './SignUp'
-import Game from './Game'
+import SignUp from "./SignUp";
+import Game from "./Game";
+import Private from "./Private";
+import { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState("SignUp");
+  const switchPage = (newPage) => {
+    setPage(newPage);
+  };
 
   return (
-    <div className='app'>
-      <SignUp></SignUp>
-      <Game></Game>
+    <div className="app">
+      {page == "SignUp" && <SignUp onChange={switchPage}></SignUp>}
+
+      {page == "Game" && <Game onChange={switchPage}></Game>}
+
+      {page == "Private" && <Private onChange={switchPage}></Private>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
