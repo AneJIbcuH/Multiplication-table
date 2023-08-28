38bed7fc82b3245e0e76c612f77c331dc2af8fba
AneJIbcuH <114512931+AneJIbcuH@users.noreply.github.com>
2023-08-28 19:29:59 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import SignUp from "./SignUp";
 import Game from "./Game";
 import Private from "./Private";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ToLogIn from "./ToLogIn";
+import ToSignUp from "./ToSignUp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
       <div className="app">
-
         <Routes>
-
           <Route path="/" element={<SignUp />}></Route>
-
+          <Route path="/ToSignUp" element={<ToSignUp />}></Route>
+          <Route path="/ToLogIn" element={<ToLogIn />}></Route>
           <Route path="/Game" element={<Game />}></Route>
-
           <Route path="/Private" element={<Private />}></Route>
-
         </Routes>
       </div>
     </Router>
-    
   );
 }
 
